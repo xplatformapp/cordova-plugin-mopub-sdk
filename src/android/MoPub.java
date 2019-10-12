72ed6349beddea180312822cca6ef65e2c018426
Rahadur Rahman <get.rahadur@gmail.com>
2019-10-12 20:22:13 +0600

    Methods are now callable from app

--- src/android/MoPub.java
+++ src/android/MoPub.java
@@ -7,26 +7,150 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.mopub.common.MoPubReward;
+import com.mopub.common.SdkConfiguration;
+import com.mopub.common.SdkInitializationListener;
+import com.mopub.common.logging.MoPubLog;
+import com.mopub.mobileads.MoPubErrorCode;
+import com.mopub.mobileads.MoPubInterstitial;
+import com.mopub.mobileads.MoPubRewardedVideoListener;
+import com.mopub.mobileads.MoPubRewardedVideoManager;
+import com.mopub.mobileads.MoPubRewardedVideos;
+import com.mopub.mobileads.MoPubView;
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class MoPub extends CordovaPlugin {
 
+    private MoPubView mMoPubView;
+
+    private MoPubInterstitial mInterstitial;
+
+    private MoPubReward mRewardedVideo;
+
+    private String rewardAdId;
+
+    private boolean isBannerPrepare = false;
+
+    private boolean isInterstitialLoaded = false;
+
+    private boolean isRewardVideoLoaded = false;
+
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if (action.equals("showBanner")) {
+            JSONObject options = args.getJSONObject(0);
+            this.showBanner(options, callbackContext);
+            return true;
+        }
+        if(action.equals("hideBanner")) {
+            JSONObject options = args.getJSONObject(0);
+            this.showBanner(options, callbackContext);
+            return true;
+        }
+        if(action.equals("resumeBanner")) {
+            JSONObject options = args.getJSONObject(0);
+            this.showBanner(options, callbackContext);
+            return true;
+        }
+        if(action.equals("removeBanner")) {
+            JSONObject options = args.getJSONObject(0);
+            this.showBanner(options, callbackContext);
+            return true;
+        }
+        if(action.equals("prepareInterstitial")) {
+            JSONObject options = args.getJSONObject(0);
+            this.showBanner(options, callbackContext);
+            return true;
+        }
+        if(action.equals("showInterstitial")) {
+            JSONObject options = args.getJSONObject(0);
+            this.showBanner(options, callbackContext);
+            return true;
+        }
+        if(action.equals("prepareRewardVideo")) {
+            JSONObject options = args.getJSONObject(0);
+            this.showBanner(options, callbackContext);
+            return true;
+        }
+        if(action.equals("showRewardVideo")) {
+            JSONObject options = args.getJSONObject(0);
+            this.showBanner(options, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
+    private void showBanner(JSONObject options,  CallbackContext callbackContext) throws JSONException {
+        if (options != null && options.length() > 0) {
+            callbackContext.success(options);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
+
+
+    private void hideBanner(JSONObject options,  CallbackContext callbackContext) throws JSONException {
+        if (options != null && options.length() > 0) {
+            callbackContext.success(options);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+
+    private void resumeBanner(JSONObject options,  CallbackContext callbackContext) throws JSONException {
+        if (options != null && options.length() > 0) {
+            callbackContext.success(options);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+
+    private void removeBanner(JSONObject options,  CallbackContext callbackContext) throws JSONException {
+        if (options != null && options.length() > 0) {
+            callbackContext.success(options);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+
+    private void prepareInterstitial(JSONObject options,  CallbackContext callbackContext) throws JSONException {
+        if (options != null && options.length() > 0) {
+            callbackContext.success(options);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+
+    private void showInterstitial(JSONObject options,  CallbackContext callbackContext) throws JSONException {
+        if (options != null && options.length() > 0) {
+            callbackContext.success(options);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private void prepareRewardVideo(JSONObject options,  CallbackContext callbackContext) throws JSONException {
+        if (options != null && options.length() > 0) {
+            callbackContext.success(options);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+
+    private void showRewardVideo(JSONObject options,  CallbackContext callbackContext) throws JSONException {
+        if (options != null && options.length() > 0) {
+            callbackContext.success(options);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
 }
--- www/MoPub.js
+++ www/MoPub.js
@@ -1,5 +1,37 @@
-var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'MoPub', 'coolMethod', [arg0]);
+exports.coolMethod = function(arg0, success, error) {
+  exec(success, error, "MoPub", "coolMethod", [arg0]);
+};
+
+exports.showBanner = function(options, success, error) {
+  exec(success, error, "MoPub", "showBanner", [options]);
+};
+
+exports.hideBanner = function(options, success, error) {
+  exec(success, error, "MoPub", "hideBanner", [options]);
+};
+
+exports.resumeBanner = function(options, success, error) {
+  exec(success, error, "MoPub", "resumeBanner", [options]);
+};
+
+exports.removeBanner = function(options, success, error) {
+  exec(success, error, "MoPub", "removeBanner", [options]);
+};
+
+exports.prepareInterstitial = function(options, success, error) {
+  exec(success, error, "MoPub", "prepareInterstitial", [options]);
+};
+
+exports.showInterstitial = function(options, success, error) {
+  exec(success, error, "MoPub", "showInterstitial", [options]);
+};
+
+exports.prepareRewardVideo = function(options, success, error) {
+  exec(success, error, "MoPub", "prepareRewardVideo", [options]);
+};
+
+exports.showRewardVideo = function(options, success, error) {
+  exec(success, error, "MoPub", "showRewardVideo", [options]);
 };
